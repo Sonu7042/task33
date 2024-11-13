8e4e28e81d8ca3f8e8c5d98ded094d276034999e
Sonu7042 <sonusinghmourya100@gmail.com>
2024-11-13 22:47:44 +0530

    updated

--- client/src/component/Api.jsx
+++ client/src/component/Api.jsx
@@ -1,5 +1,6 @@
-const api = "http://localhost:9000"
-// const api = "https://task33-roan.vercel.app" 
+// const api = "http://localhost:9000"
+
+const api= "https://task33-roan.vercel.app"
 
 
 export default api
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://task33-ne7m.vercel.app",
     credentials: true,
   })
 );
